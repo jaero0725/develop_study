87d8b2c4ecbacdc55583dd0321afa5de3290d502
choijaeho <dhqkgbs@naver.com>
2023-05-03 01:04:57 +0900

    add : jdbcBatchItemWriter - bulk Insert processing

--- spring/spring_batch/spring-batch-mysql/src/main/java/fc/spring/batch/springbatch/part03/ItemWriterConfiguration.java
+++ spring/spring_batch/spring-batch-mysql/src/main/java/fc/spring/batch/springbatch/part03/ItemWriterConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
@@ -17,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,12 +31,14 @@ public class ItemWriterConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     @Bean
     public Job itemWriterJob() throws Exception {
         return jobBuilderFactory.get("itemWriterJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.csvFileItemWriterStep())
+                .next(this.jdbcBatchItemWriterStep())
                 .build();
     }
 
@@ -43,6 +49,26 @@ public class ItemWriterConfiguration {
                 .reader(itemReader())
                 .writer(csvFileItemWriter())
                 .build();
+    }
+
+    @Bean
+    public Step jdbcBatchItemWriterStep(){
+        return stepBuilderFactory.get("jdbcItemWriterStep")
+                .<Person, Person> chunk(10)
+                .reader(itemReader())
+                .writer(jdbcBatchItemWriter())
+                .build();
+    }
+
+    private ItemWriter<Person> jdbcBatchItemWriter() {
+        JdbcBatchItemWriter<Person> itemWriter=  new JdbcBatchItemWriterBuilder<Person>()
+                .dataSource(dataSource)
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("insert into person(name, age, address) values (:name, :age, :address)")
+                .build();
+
+        itemWriter.afterPropertiesSet();
+        return itemWriter;
     }
 
     private ItemWriter<Person> csvFileItemWriter() throws Exception{
--- spring/spring_batch/spring-batch-mysql/src/main/java/fc/spring/batch/springbatch/part03/Person.java
+++ spring/spring_batch/spring-batch-mysql/src/main/java/fc/spring/batch/springbatch/part03/Person.java
@@ -14,7 +14,7 @@ import javax.persistence.Id;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
