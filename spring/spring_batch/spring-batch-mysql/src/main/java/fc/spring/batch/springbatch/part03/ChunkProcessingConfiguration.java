6f206612b16dfaaddc4d9438046ff50a17e7c955
choijaeho <dhqkgbs@naver.com>
2023-05-02 21:43:38 +0900

    update : ChunkProcessingConfiguration.java

---
+++ spring/spring_batch/spring-batch-mysql/src/main/java/fc/spring/batch/springbatch/part03/ChunkProcessingConfiguration.java
@@ -0,0 +1,121 @@
+package fc.spring.batch.springbatch.part03;
+
+import io.micrometer.core.instrument.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ChunkProcessingConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job chunkProcessingJob() {
+        return jobBuilderFactory.get("chunkProcessingJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.taskBaseStep())
+                .next(this.chunkBaseStep(null))
+                .build();
+    }
+
+    // null 을 return 할떄까지 반복
+    @Bean
+    @JobScope
+    public Step chunkBaseStep(@Value("#{jobParameters[chunkSize]}") String chunkSize){
+        return stepBuilderFactory.get("chunkBaseStep")
+                /*
+                    paging 처리 처럼 nice 하게 처리할 수 있다.
+                    100개의 데이터를 10개씩 나눈다는 뜻.
+                    <input type , output type>
+                 */
+                .<String, String>chunk(StringUtils.isNotEmpty(chunkSize) ? Integer.parseInt(chunkSize) : 10)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemReader<String> itemReader() {
+        return new ListItemReader<>(getItems());
+    }
+
+    private ItemProcessor<String, String> itemProcessor(){
+        return item -> item +", spring batch";
+    }
+
+    private ItemWriter <String> itemWriter(){
+        //return items -> items.forEach(log::info);
+        return items -> log.info("chunk item size : {}", items.size());
+    }
+
+    @Bean
+    public Step taskBaseStep() {
+        return stepBuilderFactory.get("taskBaseStep")
+                .tasklet(this.tasklet(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet tasklet(@Value("#{jobParameters[chunksize]}") String value) {
+        List<String> items = getItems();
+
+        return ((contribution, chunkContext) -> {
+            StepExecution stepExecution = contribution.getStepExecution();
+            JobParameters jobParameters = stepExecution.getJobParameters();
+
+            // value = jobParameters.getString("chunkSize", "10");
+            int chunkSize = StringUtils.isNotEmpty(value) ? Integer.parseInt(value) : 10;
+
+            int fromIndex = stepExecution.getReadCount();
+            int toIndex = fromIndex + chunkSize;
+
+            if (fromIndex >= items.size()) {
+                return RepeatStatus.FINISHED;
+            }
+
+            // subList 메서드로 패이징 처리가능
+            List<String> subList = items.subList(fromIndex, toIndex);
+
+            log.info("task item size : {}", subList.size());
+
+            stepExecution.setReadCount(toIndex);
+
+            return RepeatStatus.CONTINUABLE;
+        });
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            items.add(i + " Hello");
+        }
+        return items;
+    }
+}
+
