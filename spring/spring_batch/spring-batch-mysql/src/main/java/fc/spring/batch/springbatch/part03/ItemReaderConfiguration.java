263def98c4e537b7a63f8998f8bbc54b065bee53
choijaeho <dhqkgbs@naver.com>
2023-05-03 00:17:13 +0900

    add : JdbcCursorItemReader

--- spring/spring_batch/spring-batch-mysql/src/main/java/fc/spring/batch/springbatch/part03/ItemReaderConfiguration.java
+++ spring/spring_batch/spring-batch-mysql/src/main/java/fc/spring/batch/springbatch/part03/ItemReaderConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -16,6 +18,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,6 +33,7 @@ public class ItemReaderConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     @Bean
     public Job itemReaderJob() throws Exception {
@@ -37,6 +41,7 @@ public class ItemReaderConfiguration {
                 .incrementer(new RunIdIncrementer())
                 .start(this.customItemReaderStep())
                 .next(this.csvFileStep())
+                .next(this.jdbcStep())
                 .build();
     }
 
@@ -57,6 +62,27 @@ public class ItemReaderConfiguration {
                 .build();
     }
 
+    @Bean
+    public Step jdbcStep() throws Exception{
+        return stepBuilderFactory.get("jdbcStep")
+                .<Person, Person> chunk(10)
+                .reader(this.jdbcCursorItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private JdbcCursorItemReader<Person> jdbcCursorItemReader() throws Exception {
+        JdbcCursorItemReader <Person> itemReader=  new JdbcCursorItemReaderBuilder<Person>()
+                .name("jdbcCursorItemReader")
+                .dataSource(dataSource)
+                .sql("select id, name, age, address from person")
+                .rowMapper((rs, rowNum) -> new Person(rs.getInt(1),rs.getString(2),
+                        rs.getString(3),rs.getString(4)))
+                .build();
+
+        itemReader.afterPropertiesSet();    // 정상적으로 설정됐는지 검증하는 메서드
+        return itemReader;
+    }
     private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
 
         // csv -> Pesron 객체로 매핑
