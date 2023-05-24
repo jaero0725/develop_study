ec06c4106014f7a8ef784dd35abc7644848ef0c3
choijaeho <dhqkgbs@naver.com>
2023-05-24 14:54:11 +0900

    init : jpa example code update

---
+++ spring/spring_remind/springboot_quickStart/src/test/java/com/example/springboot_quickstart/SpringbootQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuickStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
